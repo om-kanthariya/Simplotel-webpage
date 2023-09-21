bb4e782e9b0b9dcba0ff715f7470d54397736b44
om-kanthariya <119924838+om-kanthariya@users.noreply.github.com>
2023-09-21 16:46:22 +0530

    New Files

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var navLinks = document.querySelector('.nav-links');
+    if (navLinks.style.display === 'block') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'block';
+    }
+}
